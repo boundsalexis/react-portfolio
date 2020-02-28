2e3aca5e89fa0fa11afb4ed7ff40928cc75d29a6
Alexis <lxs.bnds@gmail.com>
2020-02-28 10:37:44 -0800

    Updates

---
+++ precache-manifest.84e9a644cc4e0e3099b78c867a057177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd4ea1dd89ff915308e2ed061fcedf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5654e256e33e7bee6923",
+    "url": "/static/css/main.70c6695f.chunk.css"
+  },
+  {
+    "revision": "d87dadd8880422f2d7d0",
+    "url": "/static/js/2.57674f4f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.57674f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5654e256e33e7bee6923",
+    "url": "/static/js/main.94771a5d.chunk.js"
+  },
+  {
+    "revision": "3ac27118c52e75d53249",
+    "url": "/static/js/runtime-main.4c2bc187.js"
+  }
+]);
